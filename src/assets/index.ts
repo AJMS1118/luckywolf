cb0fb9f21ba42ce289ad044bafce4932a0d6827d
Andrew <aj.quesada1990@gmail.com>
2025-03-23 04:35:08 -0400

    creating the navbars menus

---
+++ src/assets/index.ts
@@ -0,0 +1,33 @@
+import logo from "./Logo.svg";
+import menu_button from "./menu-button.svg";
+import sports from "./sport.svg";
+import casino from "./casino.svg";
+import search_icon from "./search-icon.svg";
+import lottery from "./lottery.svg";
+import loterry_1 from "./lottery-1.png";
+import favorites from "./favorites.png";
+import sport from "./sport.png";
+import recent from "./recent.png";
+import casino_1 from "./casino-1.svg";
+import button_group from "./button-group.svg";
+import flat from "./flat.png";
+import support from "./support.svg";
+import connection from "./connection.svg";
+
+export {
+  logo,
+  menu_button,
+  casino,
+  sports,
+  search_icon,
+  lottery,
+  loterry_1,
+  favorites,
+  recent,
+  casino_1,
+  sport,
+  button_group,
+  flat,
+  support,
+  connection,
+};
---
+++ src/components/small-navbar/SmallNavbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { AreaComponent } from "../ui/area-component/AreaComponent";
+import styles from "./small-navbar.module.css";
+import {
+  loterry_1,
+  lottery,
+  sport,
+  casino_1,
+  recent,
+  favorites,
+  button_group,
+  flat,
+  support,
+  connection,
+} from "@/assets";
+
+export const SmallNavbar = () => {
+  return (
+    <>
+      <div className={styles.small_navbar_container}>
+        <div className={styles.area_component_container}>
+          <AreaComponent icon={lottery} label="Lottery" width={38} height={38} />
+          <AreaComponent icon={favorites} label="Favorites" width={34} height={38} />
+          <AreaComponent icon={recent} label="Recent" width={38} height={38} />
+          <AreaComponent icon={casino_1} label="Casino" width={32} height={38} />
+          <AreaComponent icon={sport} label="Sport" width={38} height={38} />
+          <AreaComponent icon={loterry_1} label="Lottery" width={38} height={38} />
+        </div>
+        <div className={styles.button_group_container}>
+          <Image src={button_group} alt="" />
+        </div>
+        <div className={styles.section_3}>
+          <div className={styles.flat}>
+            <Image src={flat} alt="" />
+          </div>
+          <div>
+            <Image src={support} alt="" />
+          </div>
+          <div>
+            <Image src={connection} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
